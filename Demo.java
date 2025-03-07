a8ad8706efea230417c6989b0d38654bab1a0782
shivam <shivampoddartech@gmail.com>
2025-03-07 23:20:27 +0530

    Collection

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class Demo {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20.20);
+		al.add("Shivam");
+		al.add('a');
+		System.out.println(al);
+	}
+}
